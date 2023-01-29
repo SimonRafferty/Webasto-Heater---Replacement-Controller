933bbae2ccd9e715d59c440851a150dc6661fd61
Simon Rafferty <simon1@psi-design.co.uk>
2023-01-29 13:03:15 +0000

    Fixed problem with temp dependent prime ratio

--- prime_ratio.ino
+++ prime_ratio.ino
@@ -8,4 +8,4 @@ float prime_ratio(float ambient_temperature) {
   float steps = prime_high_temp-prime_low_temp;
   float fuel_steps = (prime_low_temp_fuelrate-prime_high_temp_fuelrate)/steps;  
   return prime_low_temp_fuelrate-((exhaust_temp-prime_low_temp)*fuel_steps);
-}
+} 

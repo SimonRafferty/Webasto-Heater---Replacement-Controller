eb0be69bfbb2fe50ad155e76076aabc10fe04fbb
Simon Rafferty <simon1@psi-design.co.uk>
2023-11-18 17:35:33 +0000

    Delete fan.ino

--- fan.ino
+++
@@ -1,8 +0,0 @@
-void burn_fan() {
-  // the webasto fan runs on 10v so we need to that into account
-  int percent_map = mapf(fan_speed, 0, 100, 0, 196); // Assuming a 13V Input, 196 gives 10V
-
-  analogWrite(burn_fan_pin, percent_map);
-  //ledcWrite(air_channel, percent_map);
-  //pwm.analogWrite(burn_fan_pin, percent_map);
-}
